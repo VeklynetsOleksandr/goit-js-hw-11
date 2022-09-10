b915e15b7f1f448e44210a40bd2bb71cb71eea9a
VeklynetsOleksandr <98841842+VeklynetsOleksandr@users.noreply.github.com>
2022-09-10 14:07:07 +0300

    update task

--- src/js/fetch-images.js
+++ src/js/fetch-images.js
@@ -2,11 +2,11 @@ import axios from 'axios';
 export { fetchImages };
 
 axios.defaults.baseURL = 'https://pixabay.com/api/';
-const KEY = '25766392-01b12b6ed5ab34bc2910d9c3e';
+const KEY = '29832813-50bebe44f13572e25ed67f5b6';
 
 async function fetchImages(query, page, perPage) {
   const response = await axios.get(
-    `?key=${KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`,
+    `?key=${KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`
   );
   return response;
 }
